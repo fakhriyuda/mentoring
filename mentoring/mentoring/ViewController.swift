93daf390ad3a3f3d93da7c07ccff244c3d449333
fakhriyuda <37844797+fakhriyuda@users.noreply.github.com>
2021-06-28 19:01:43 +0700

    first commit
    first commit

---
+++ mentoring/mentoring/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mentoring
+//
+//  Created by Fakhri Yuda Rabbani on 28/06/21.
+//  Copyright © 2021 Yudstudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
